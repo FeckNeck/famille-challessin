38390e4b6f0822970eeb9b96777f38a03f10e21d
feckneck <math.dousse@hotmail.fr>
2024-03-10 14:28:17 +0100

    chore: init project

---
+++ inertia/components/footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer>
+    <div class="container">"Footer content here.</div>
+  </footer>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ inertia/components/header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header>
+    <nav class="container">
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss"></style>
