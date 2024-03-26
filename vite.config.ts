3d123c8cecbfb9802fcb6be156c89a8cb3eb482b
feckneck <math.dousse@hotmail.fr>
2024-03-26 11:53:29 +0100

    feat(vite-config): update alis value

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
+import { resolve } from 'node:path'
 
 export default defineConfig({
   plugins: [
@@ -17,7 +18,8 @@ export default defineConfig({
    */
   resolve: {
     alias: {
-      '~/': `${getDirname(import.meta.url)}/inertia/`,
+      '~/': `${resolve(getDirname(import.meta.url), 'inertia')}/`,
+      // '~/': `${resolve(getDirname(import.meta.url), '.')}/`,
     },
   },
 })
