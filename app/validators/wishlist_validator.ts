2a6b9529d5a3f0da3f23c7aabea4a854fcd229c0
feckneck <math.dousse@hotmail.fr>
2024-03-27 17:41:42 +0100

    feat(wishlist): update validator with correct date format

--- app/validators/wishlist_validator.ts
+++ app/validators/wishlist_validator.ts
@@ -1,10 +1,14 @@
 import vine from '@vinejs/vine'
+import { DateTime } from 'luxon'
 
 export const wishlistValidator = vine.compile(
   vine.object({
     title: vine.string().trim(),
     description: vine.string().trim().optional(),
-    event_date: vine.date().optional(),
+    eventDate: vine
+      .date()
+      .optional()
+      .transform((value) => DateTime.fromJSDate(value)),
     imageUrl: vine.string().trim().optional(),
     isPublic: vine.boolean().optional(),
   })
