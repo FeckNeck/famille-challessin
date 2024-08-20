c2565eb1ed3be352bd1c40b49c296ea3250fb730
feckneck <math.dousse@hotmail.fr>
2024-08-20 19:08:01 +0200

    feat(design-system): add Button

---
+++ inertia/components/ui/button.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps<{
+  color?: 'cyan' | 'violet' | 'red' | 'yellow' | 'lime'
+  href?: string
+  size?: 'small' | 'medium'
+}>()
+
+const buttonOrLink = computed(() => {
+  return props.href ? Link : 'button'
+})
+</script>
+
+<template>
+  <Component v-bind="$attrs" :is="buttonOrLink" class="button" :class="[props.color, props.size]">
+    <slot />
+  </Component>
+</template>
+
+<style scoped>
+.button {
+  --bg-color: transparant;
+  background-color: var(--bg-color);
+  border-radius: var(--rounded);
+  border: 2px solid var(--gray-800);
+  box-shadow: var(--shadow-small);
+  display: inline-block;
+  cursor: pointer;
+  font-size: var(--text-base);
+  font-weight: bold;
+  padding: 0.75rem 1.25rem;
+  transition: background-color 100ms ease-in;
+
+  &:not(.blank):hover {
+    --bg-color: var(--white);
+  }
+
+  &.blank {
+    border: 0 !important;
+    box-shadow: none !important;
+    padding: 0;
+  }
+
+  &.small {
+    padding: 0.5rem;
+  }
+
+  &.violet {
+    --bg-color: var(--violet-300);
+
+    &:hover {
+      --bg-color: var(--violet-500);
+    }
+  }
+
+  &.cyan {
+    --bg-color: var(--cyan-300);
+
+    &:hover {
+      --bg-color: var(--cyan-500);
+    }
+  }
+
+  &.yellow {
+    --bg-color: var(--yellow-300);
+
+    &:hover {
+      --bg-color: var(--yellow-500);
+    }
+  }
+
+  &.red {
+    --bg-color: var(--red-300);
+
+    &:hover {
+      --bg-color: var(--red-500);
+    }
+  }
+
+  &.lime {
+    --bg-color: var(--lime-300);
+
+    &:hover {
+      --bg-color: var(--lime-500);
+    }
+  }
+}
+</style>
