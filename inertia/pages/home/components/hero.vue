85e3d67604e7e5122fe14d8edba9ff282e2501c9
feckneck <math.dousse@hotmail.fr>
2024-09-09 19:36:54 +0200

    feat(home): add 3D model

--- inertia/pages/home/components/hero.vue
+++ inertia/pages/home/components/hero.vue
@@ -1,47 +1,49 @@
 <script setup lang="ts">
 import { Gift } from 'lucide-vue-next'
-import Button from '../../../components/ui/button.vue'
+import Button from '~/components/ui/button.vue'
 </script>
 
 <template>
   <div class="relative py-24 mb-14">
-    <div class="container">
-      <div class="hero">
-        <div class="hero__title">
-          <h1>
-            Listes de souhaits <br />
-            de la famille <span>challessin</span>
-          </h1>
-          <Button color="yellow" href="wishlists/create">
-            <div class="d-flex items-center g-2">
-              <span>Créer une liste</span>
-              <Gift />
-            </div>
-          </Button>
+    <div class="hero">
+      <div class="container">
+        <div class="hero__content">
+          <div class="hero__content__title">
+            <h1>
+              Listes de souhaits <br />
+              de la famille <span>challessin</span>
+            </h1>
+            <Button color="yellow" href="wishlists/create">
+              <div class="d-flex items-center g-2">
+                <span>Créer une liste</span>
+                <Gift />
+              </div>
+            </Button>
+          </div>
+          <model-viewer
+            alt="AdonisJS 3d logo"
+            class="hero__content__model-viewer"
+            :class="{
+              '--hidden': modelViewerScriptLoaded.value === false,
+              '--visible': modelViewerScriptLoaded.value === true,
+            }"
+            auto-rotate
+            disable-zoom
+            interaction-prompt="none"
+            orientation="0deg 0deg 0deg"
+            auto-rotate-delay="400"
+            rotation-per-second="30deg"
+            disable-pan
+            src="/saint-vulbas.glb"
+            camera-controls
+            touch-action="pan-y"
+            camera-orbit="30.24deg 95.94deg 18.36m"
+            field-of-view="30deg"
+            shadow-intensity="0.6"
+            shadow-softness="1"
+            tone-mapping="commerce"
+          />
         </div>
-        <model-viewer
-          alt="AdonisJS 3d logo"
-          class="hero__model-viewer"
-          :class="{
-            '--hidden': modelViewerScriptLoaded.value === false,
-            '--visible': modelViewerScriptLoaded.value === true,
-          }"
-          auto-rotate
-          disable-zoom
-          interaction-prompt="none"
-          orientation="0deg 0deg 0deg"
-          auto-rotate-delay="400"
-          rotation-per-second="30deg"
-          disable-pan
-          src="/titou.glb"
-          camera-controls
-          touch-action="pan-y"
-          camera-orbit="30.24deg 95.94deg 18.36m"
-          field-of-view="30deg"
-          shadow-intensity="0.6"
-          shadow-softness="1"
-          tone-mapping="commerce"
-        />
       </div>
     </div>
   </div>
@@ -49,76 +51,99 @@ import Button from '../../../components/ui/button.vue'
 
 <style scoped lang="scss">
 .hero {
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
+  &:before {
+    position: absolute;
+    z-index: 0;
+    content: '';
+    top: -15%;
+    bottom: 0%;
+    height: 130%;
+    width: 100%;
+    pointer-events: none;
+    // background:
+    //   url(@/assets/noise.webp) repeat,
+    //   linear-gradient(83.21deg, var(--cyan-500) 0%, var(--violet-500) 100%);
+    background: var(--violet-300);
+    background-blend-mode: overlay;
+    -webkit-mask-image: radial-gradient(rgba(0, 0, 0, 1), transparent 70%);
+    mask-image: radial-gradient(rgba(0, 0, 0, 1), transparent 70%);
+    opacity: 0.5;
+  }
 
-  &__title {
+  &__content {
     display: flex;
+    justify-content: space-between;
     flex-direction: column;
     align-items: center;
-    text-align: center;
-    gap: 1rem;
+    position: relative;
+    gap: 2rem;
 
-    @media (min-width: 768px) {
-      align-items: flex-start;
-      text-align: left;
-    }
+    &__title {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      text-align: center;
+      gap: 1rem;
 
-    h1 {
-      color: var(--gray-800);
-      font-size: var(--text-h1);
-      font-weight: normal;
-      line-height: 64px;
+      @media (min-width: 768px) {
+        align-items: flex-start;
+        text-align: left;
+      }
 
-      span {
-        position: relative;
-        display: inline-block;
+      h1 {
+        color: var(--gray-800);
+        font-size: var(--text-h1);
+        font-weight: normal;
+        line-height: 64px;
 
-        &:before {
-          background-color: var(--red-300);
-          content: '';
-          height: 95%;
-          margin-left: -2.5%;
-          position: absolute;
-          transform: rotate(-2deg);
-          transition: transform 300ms ease-in-out;
-          width: 105%;
-          will-change: transform;
-          z-index: -1;
-        }
+        span {
+          position: relative;
+          display: inline-block;
+          z-index: 1;
 
-        &:hover:before {
-          transform: rotate(0deg);
+          &:before {
+            background-color: var(--red-300);
+            content: '';
+            height: 95%;
+            margin-left: -2.5%;
+            position: absolute;
+            transform: rotate(-2deg);
+            transition: transform 300ms ease-in-out;
+            width: 105%;
+            will-change: transform;
+            z-index: -1;
+          }
+
+          &:hover:before {
+            transform: rotate(0deg);
+          }
         }
       }
     }
-  }
 
-  &__model-viewer {
-    display: block;
-    height: 27.5rem;
-    flex: 1;
-    transition: opacity 4s ease-in-out;
-    --progress-bar-color: transparent;
+    &__model-viewer {
+      display: block;
+      height: 27.5rem;
+      flex: 1;
+      transition: opacity 4s ease-in-out;
+      --progress-bar-color: transparent;
 
-    &.--hidden {
-      opacity: 0;
+      &.--hidden {
+        opacity: 0;
+      }
+
+      &.--visible {
+        opacity: 1;
+      }
+
+      @media (max-width: 640px) {
+        display: none;
+      }
     }
 
-    &.--visible {
-      opacity: 1;
+    @media (min-width: 768px) {
+      flex-direction: row;
     }
-
-    @media (max-width: 640px) {
-      display: none;
-    }
-  }
-
-  @media (min-width: 768px) {
-    flex-direction: row;
   }
 }
 </style>
