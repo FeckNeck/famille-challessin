46653ecf24b764a453c4512c93c8c8d0842eefcf
feckneck <math.dousse@hotmail.fr>
2024-10-02 20:23:19 +0200

    feat(edit-wishlist): create component for category creation

--- inertia/pages/wishlist/edit/main.vue
+++ inertia/pages/wishlist/edit/main.vue
@@ -1,46 +1,14 @@
 <script setup lang="ts">
-import { useForm } from '@inertiajs/vue3'
+import Category from './components/category.vue'
+import CreateCategory from './components/create_category.vue'
 import Hero from './components/hero.vue'
 import Layout from '~/layouts/default.vue'
 import type { WishlistTheme, Wishlist } from '~/types'
-import Input from '~/components/ui/input.vue'
-import Field from '~/components/ui/field.vue'
-import Button from '~/components/ui/button.vue'
-import Collapsible from '~/components/ui/collapsible.vue'
-import Gift from './components/gift.vue'
-import Category from './components/category.vue'
 
 const props = defineProps<{
   themes: WishlistTheme[]
   wishlist: Wishlist
 }>()
-
-const categoryForm = useForm({
-  name: '',
-})
-
-const giftForm = useForm({
-  url: '',
-})
-
-function submitCategory() {
-  if (categoryForm.processing) return
-
-  categoryForm.post(`/wishlists/${props.wishlist.id}/categories`, {
-    preserveScroll: true,
-    onSuccess: () => {
-      categoryForm.name = ''
-    },
-  })
-}
-
-function submitGift() {
-  if (giftForm.processing) return
-
-  giftForm.post(`/gifts/scrap`, {
-    preserveScroll: true,
-  })
-}
 </script>
 
 <template>
@@ -51,12 +19,7 @@ function submitGift() {
         <Hero :themes="props.themes" :wishlist="props.wishlist" />
 
         <!-- Create category-->
-        <form @submit.prevent="submitCategory()" class="d-flex items-center g-4 pt-4">
-          <Field label="Ajouter une catégorie" :error="categoryForm.errors.name" class="grow">
-            <Input v-model:input="categoryForm.name" placeholder="Nom de la catégorie" />
-          </Field>
-          <Button color="yellow" size="small">Ajouter</Button>
-        </form>
+        <CreateCategory :wishlistId="props.wishlist.id" />
 
         <!-- Categories -->
         <div class="d-flex column g-4" v-auto-animate>
